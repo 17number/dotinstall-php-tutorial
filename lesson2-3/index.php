0ca22b8a9ff63ab00d41a0a63f5d4456a50a6a7b
r17n <4716989+17number@users.noreply.github.com>
2019-09-12 18:29:27 +0900

    lesson2-3

---
+++ lesson2-3/index.php
@@ -0,0 +1,20 @@
+<?php
+  // single line comment
+  # single line comment
+  /*
+    multi line comment
+   */
+  echo "Hello from top"
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <p>Hello world <?php echo " from php"; ?></p>
+</body>
+</html>
